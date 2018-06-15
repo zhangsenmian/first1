fbb2244fb2eac880dca5a251272a181d2b38491a
zhengzewei <1763839483@qq.com>
2018-06-15 11:25:28 +0800

    zheng-meituan4

--- meituan/src/com/meituan/damian/User.java
+++ meituan/src/com/meituan/damian/User.java
@@ -1,20 +1,19 @@
 package com.meituan.damian;
 
-import java.util.Date;
+import java.sql.Date;
 
 public class User {
-	private String id; // 用户编号
-	private String username; // 用户姓名
-	private String password; // 用户密码
-	private String gender; // 用户性别
-	private String email; // 用户邮箱
-	private String telephone; // 用户联系电话
-	private String introduce; // 用户介绍
-	private String activeCode; // 激活码
-	private String role; // 用户角色
-	private int state; // 用户状态
-    private Date registTime;// 注册时间
-	
+	private String id;
+	private String username;
+	private String password;
+	private String gender;
+	private String email;
+	private String telephone;
+	private String introduce;
+	private String activeCode;
+	private String role;
+	private int state;
+	private Date registTime;
 	public String getId() {
 		return id;
 	}
@@ -81,5 +80,5 @@ public class User {
 	public void setRegistTime(Date registTime) {
 		this.registTime = registTime;
 	}
-
+	
 }
--- meituan/src/com/meituan/dao/UserDao.java
+++ meituan/src/com/meituan/dao/UserDao.java
@@ -3,12 +3,11 @@ package com.meituan.dao;
 import java.sql.SQLException;
 import java.util.List;
 
+
+
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
-
-import org.apache.commons.dbutils.handlers.ColumnListHandler;
-import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import com.meituan.damian.User;
 import com.meituan.util.C3P0Util;
